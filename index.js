e034e866a950af6165de69655a37f63384867909
Brian Mitchell <bman4789@gmail.com>
2021-01-25 14:11:08 -0600

    Try leaving a comment on a PR

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+  const githubToken = core.getInput('GITHUB_TOKEN');
+
   const context = github.context;
 
-  if (context.payload.pull_request_review == null) {
+  if (context.payload.pull_request == null) {
     core.setFailed('No pull request review found.');
     return;
   }
 
-  core.debug(context.payload.pull_request_review);
+  core.debug(JSON.stringify(context.payload.pull_request, null, 2));
+
+  const octokit = github.getOctokit(githubToken);
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: context.payload.pull_request.number,
+    body: "This is the coolest Pull Request I've ever seen!",
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
