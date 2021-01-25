433dd844c6acef9ccab2f77cb849e949515cfb1e
Brian Mitchell <bman4789@gmail.com>
2021-01-25 14:19:06 -0600

    Wrap the action in a function to work with async/await

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  const githubToken = core.getInput('GITHUB_TOKEN');
+async function action() {
+  try {
+    const githubToken = core.getInput('GITHUB_TOKEN');
 
-  const context = github.context;
+    const context = github.context;
 
-  if (context.payload.pull_request == null) {
-    core.setFailed('No pull request review found.');
-    return;
+    if (context.payload.pull_request == null) {
+      core.setFailed('No pull request review found.');
+      return;
+    }
+
+    core.debug(JSON.stringify(context.payload.pull_request, null, 2));
+
+    const octokit = github.getOctokit(githubToken);
+    await octokit.issues.createComment({
+      ...context.repo,
+      issue_number: context.payload.pull_request.number,
+      body: "This is the coolest Pull Request I've ever seen!",
+    });
+  } catch (error) {
+    core.setFailed(error.message);
   }
-
-  core.debug(JSON.stringify(context.payload.pull_request, null, 2));
-
-  const octokit = github.getOctokit(githubToken);
-  await octokit.issues.createComment({
-    ...context.repo,
-    issue_number: context.payload.pull_request.number,
-    body: "This is the coolest Pull Request I've ever seen!",
-  });
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+action();
