99ef4c9245357a2ad31c9ed506378027b023b688
Brian Mitchell <bman4789@gmail.com>
2021-01-26 15:17:42 -0600

    Add a GIF to the comment

--- dist/index.js
+++ dist/index.js
@@ -7,10 +7,40 @@ module.exports =
 
 const core = __nccwpck_require__(186);
 const github = __nccwpck_require__(438);
+const fetch = __nccwpck_require__(467);
+
+async function fetchGif(tenorApiKey, keyword) {
+  const params = new URLSearchParams();
+  params.append('key', tenorApiKey);
+  params.append('q', keyword);
+  params.append('limit', '1');
+  params.append('contentfilter', 'medium');
+  params.append('media_filter', 'minimal');
+
+  try {
+    const response = await fetch(
+      `https://api.tenor.com/v1/search?${params.toString()}`
+    );
+    if (response.ok) {
+      const json = await response.json();
+      return json.results[0].media[0].gif.url;
+    } else {
+      return null;
+    }
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
 
 async function run() {
   try {
     const githubToken = core.getInput('GITHUB_TOKEN');
+    const tenorApiKey = core.getInput('TENOR_API_KEY');
+    const gifSuccessKeywords = core
+      .getInput('gifSuccessKeywords')
+      .split(',')
+      .map((keyword) => keyword.trim());
 
     const context = github.context;
 
@@ -27,10 +57,25 @@ async function run() {
     const octokit = github.getOctokit(githubToken);
 
     if (context.payload.review && context.payload.review.state === 'approved') {
+      let gifUrl = null;
+
+      if (tenorApiKey) {
+        const keyword =
+          gifSuccessKeywords[
+            Math.floor(Math.random() * gifSuccessKeywords.length)
+          ];
+        gifUrl = await fetchGif(tenorApiKey, keyword);
+      }
+
+      let message = 'Good job, you wrote some good code.';
+      if (gifUrl) {
+        message += `\n![GIF](${gifUrl})`;
+      }
+
       await octokit.issues.createComment({
         ...context.repo,
         issue_number: context.payload.pull_request.number,
-        body: 'Good job, you wrote some good code.',
+        body: message,
       });
     }
   } catch (error) {
--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fetch = require('node-fetch');
+
+async function fetchGif(tenorApiKey, keyword) {
+  const params = new URLSearchParams();
+  params.append('key', tenorApiKey);
+  params.append('q', keyword);
+  params.append('limit', '1');
+  params.append('contentfilter', 'medium');
+  params.append('media_filter', 'minimal');
+
+  try {
+    const response = await fetch(
+      `https://api.tenor.com/v1/search?${params.toString()}`
+    );
+    if (response.ok) {
+      const json = await response.json();
+      return json.results[0].media[0].gif.url;
+    } else {
+      return null;
+    }
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
 
 async function run() {
   try {
     const githubToken = core.getInput('GITHUB_TOKEN');
+    const tenorApiKey = core.getInput('TENOR_API_KEY');
+    const gifSuccessKeywords = core
+      .getInput('gifSuccessKeywords')
+      .split(',')
+      .map((keyword) => keyword.trim());
 
     const context = github.context;
 
@@ -20,10 +50,25 @@ async function run() {
     const octokit = github.getOctokit(githubToken);
 
     if (context.payload.review && context.payload.review.state === 'approved') {
+      let gifUrl = null;
+
+      if (tenorApiKey) {
+        const keyword =
+          gifSuccessKeywords[
+            Math.floor(Math.random() * gifSuccessKeywords.length)
+          ];
+        gifUrl = await fetchGif(tenorApiKey, keyword);
+      }
+
+      let message = 'Good job, you wrote some good code.';
+      if (gifUrl) {
+        message += `\n![GIF](${gifUrl})`;
+      }
+
       await octokit.issues.createComment({
         ...context.repo,
         issue_number: context.payload.pull_request.number,
-        body: 'Good job, you wrote some good code.',
+        body: message,
       });
     }
   } catch (error) {
