668d8002d0c71eb52a81f4777264edc4dd2a1b74
Brian Mitchell <bman4789@gmail.com>
2021-01-28 12:54:49 -0600

    Add some comments and update versioning

--- dist/index.js
+++ dist/index.js
@@ -6074,22 +6074,29 @@ function findKeyword({ state, body, fallbackApprove, fallbackChangesRequested, }
     if (random < 0.3) {
         return '';
     }
+    // lean towards negativity for changes_requested
     if (state === 'changes_requested') {
+        // if the sentiment is negative, use a negative word
         if (analysis.comparative < 0 && analysis.negative.length > 0) {
             return randomInArray(analysis.negative);
         }
+        // if it's positive, use a positive word
         if (analysis.comparative > 0 && analysis.positive.length > 0) {
             return randomInArray(analysis.positive);
         }
+        // if it's exactly neutral, use any word
         if (analysis.words.length > 0) {
             return randomInArray(analysis.words);
         }
+        // if no words match, use a fallback word
         return fallbackChangesRequested;
     }
     else {
+        // if the sentiment is positive, use a positive word
         if (analysis.comparative > 0 && analysis.positive.length > 0) {
             return randomInArray(analysis.positive);
         }
+        // if not a positive sentiment, use a fallback word
         return fallbackApprove;
     }
 }
